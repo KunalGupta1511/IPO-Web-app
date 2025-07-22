94b4865bf0ccc672198f9e932964710039210b35
KunalGupta1511 <kunal152005@gmail.com>
2025-07-22 10:52:46 +0530

    Add jwt token based login

---
+++ server/middlewares/auth.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken")
+
+const { User } = require("../models");
+const { where } = require("sequelize");
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+            try {
+                if (err) {
+                    return res.status(401).json({ error: "Unauthorized!" });
+                }
+
+                const user = await User.findOne({
+                    where: { id: payload.id },
+                    attributes: { exclude: ["password"] }
+                });
+
+                if(!user){
+                    return res.status(404).json({error : "User not found"})
+                }
+                
+                req.user = user;
+                next();
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    }
+    else {
+        return res.status(403).json({ error: "Forbidden 🛑🛑" })
+    }
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models");
+const { where } = require('sequelize');
+
+const auth = require("../middlewares/auth")
 
 router.post('/register', async (req, res) => {
     try {
@@ -28,21 +32,69 @@ router.post('/register', async (req, res) => {
         }
 
         try {
+            const doesUserExist = await User.findOne({ where: { email } })
+            if (doesUserExist) {
+                return res.status(400).json({ error: `User with email ${email} already exists` });
+            }
+
             const hashedPassword = await bcrypt.hash(password, 12)
             const newUser = await User.create({ name, email, password: hashedPassword });
             return res.status(200).json({ newUser })
         }
         catch (err) {
-            return res.status(400).json({ error: err.errors ? err.errors[0].message : err.message });
+            return res.status(400).json({ error: err.message });
         }
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
 
-router.get('/login', async (req, res) => {
-    const users = await User.findAll();
-    res.json(users);
-});
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ error: "Please enter all the fields" });
+    }
+    //Email validation
+    const emailReg = /[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/
+
+    if (!emailReg.test(email)) {
+        return res
+            .status(400)
+            .json({ error: "Incorrect Email-ID" })
+    }
+
+    try {
+        const doesUserExist = await User.findOne({where : { email }})
+
+        if (!doesUserExist) {
+            return res
+                .status(400)
+                .json({ error: "Incorrect email or password" })
+        }
+
+        const doesPasswordMatch = await bcrypt.compare(password, doesUserExist.password);
+
+        if (!doesPasswordMatch) {
+            return res
+                .status(400)
+                .json({ error: "Incorrect email or password" })
+        }
+
+        const payload = { id: doesUserExist.id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        const user = { doesUserExist, password: undefined }
+        return res.status(200).json({ token, user })
+    }
+    catch (err) {
+        console.log(err);
+        return res
+            .status(500)
+            .json({ error: err.message })
+    }
+})
 
 module.exports = router;
